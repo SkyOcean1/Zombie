1e0f62b8689bb888d882b4413fd672b0ff5514de
SkyOcean <k43205008@gmail.com>
2023-12-05 11:20:26 +0900

    좀비 서바이버 게임

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+// 플레이어 캐릭터를 사용자 입력에 따라 움직이는 스크립트
+public class PlayerMovement : MonoBehaviour {
+    float moveSpeed = 6f; // 앞뒤 움직임의 속도
+    float rotateSpeed = 270f; // 좌우 회전 속도
+
+
+    PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
+    Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
+    Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
+
+    private void Start() {
+        // 사용할 컴포넌트들의 참조를 가져오기
+        playerInput = GetComponent<PlayerInput>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
+    private void FixedUpdate() {
+        // 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
+        Rotate();
+        Move();
+
+        playerAnimator.SetFloat("Move", playerInput.move);
+    }
+
+    // 입력값에 따라 캐릭터를 앞뒤로 움직임
+    private void Move() {
+        Vector3 moveDistance = playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
+        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
+    }
+
+    // 입력값에 따라 캐릭터를 좌우로 회전
+    private void Rotate() {
+        float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
+        playerRigidbody.rotation *= Quaternion.Euler(0f, turn, 0f);
+    }
+}
